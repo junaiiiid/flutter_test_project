79fce74622fa6f3f534185f33244ada90e3a9afd
junaidtariq <junaidt805@gmail.com>
2023-03-06 20:17:06 +0500

    swapping configuration added

--- lib/view_models/home_view_model.dart
+++ lib/view_models/home_view_model.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test_project/constants/enums.dart';
 import 'package:flutter_test_project/models/configurable_dropdown_model.dart';
 import 'package:flutter_test_project/models/data_grid_model.dart';
+import 'package:collection/collection.dart';
 
 class HomeViewModel extends ChangeNotifier{
   List<String> columnLabels = ["LABEL","KEY","TYPE","VALUE"];
@@ -10,7 +11,7 @@ class HomeViewModel extends ChangeNotifier{
 
   List<DataGridModel> configurableDataGridList = [];
 
-  List<ConfigurableDropDown> get configurableDropDown  => [ConfigurableDropDown(name: "LABEL", type: type)];
+  List<String> get configurableDropDown  => ["LABEL","KEY","TYPE","VALUE"];
 
   String _labelValue = "LABEL";
   String _keyValue = "KEY";
@@ -46,19 +47,104 @@ class HomeViewModel extends ChangeNotifier{
   }
 
   void resetDropDown(){
+    configurableDataGridList = dataGridList;
     _labelValue = "LABEL";
     _keyValue = "KEY";
     _typeValue = "TYPE";
     _valueValue = "VALUE";
     notifyListeners();
   }
-  
+
   void saveConfiguration(){
+    List<DataGridModel> tempList = [];
     for(var dataGrid in configurableDataGridList){
+      ///SWAP CONFIG FOR LABEL
       switch(_labelValue){
         case "LABEL":
-          
+        ///no edit
+        break;
+        case "KEY":
+        String? temp = dataGrid.label;
+        dataGrid.label = dataGrid.key;
+        dataGrid.key = temp;
+        break;
+        case "TYPE":
+          String? temp = dataGrid.label;
+          dataGrid.label = dataGrid.type;
+          dataGrid.type = temp;
+        break;
+        case "VALUE":
+          String? temp = dataGrid.label;
+          dataGrid.label = dataGrid.value;
+          dataGrid.value = temp;
+        break;
       }
+      ///SWAP CONFIG FOR KEY
+      switch(_keyValue){
+        case "LABEL":
+          String? temp = dataGrid.key;
+          dataGrid.key = dataGrid.label;
+          dataGrid.label = temp;
+          break;
+        case "KEY":
+          ///no edit
+          break;
+        case "TYPE":
+          String? temp = dataGrid.key;
+          dataGrid.key = dataGrid.type;
+          dataGrid.type = temp;
+          break;
+        case "VALUE":
+          String? temp = dataGrid.key;
+          dataGrid.key = dataGrid.value;
+          dataGrid.value = temp;
+          break;
+      }
+      ///SWAP CONFIG FOR TYPE
+      switch(_typeValue){
+        case "LABEL":
+          String? temp = dataGrid.type;
+          dataGrid.type = dataGrid.label;
+          dataGrid.label = temp;
+          break;
+        case "KEY":
+          String? temp = dataGrid.type;
+          dataGrid.type = dataGrid.key;
+          dataGrid.key = temp;
+          break;
+        case "TYPE":
+          ///no edit
+          break;
+        case "VALUE":
+          String? temp = dataGrid.type;
+          dataGrid.type = dataGrid.value;
+          dataGrid.value = temp;
+          break;
+      }
+      ///SWAP CONFIG FOR VALUE
+      switch(_valueValue){
+        case "LABEL":
+          String? temp = dataGrid.value;
+          dataGrid.value = dataGrid.label;
+          dataGrid.label = temp;
+          break;
+        case "KEY":
+          String? temp = dataGrid.value;
+          dataGrid.value = dataGrid.key;
+          dataGrid.key = temp;
+          break;
+        case "TYPE":
+          String? temp = dataGrid.value;
+          dataGrid.value = dataGrid.type;
+          dataGrid.type = temp;
+          break;
+        case "VALUE":
+          ///no edit
+          break;
+      }
+      tempList.add(dataGrid);
     }
+    configurableDataGridList = tempList;
+    notifyListeners();
   }
 }
--- lib/widgets/my_sheets.dart
+++ lib/widgets/my_sheets.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_test_project/constants/extensions.dart';
-import 'package:flutter_test_project/models/configurable_dropdown_model.dart';
 import 'package:flutter_test_project/services/state_service.dart';
 import 'package:flutter_test_project/styles/my_colors.dart';
 import 'package:flutter_test_project/styles/my_text_styles.dart';
@@ -43,15 +42,18 @@ class MySheets {
                     MyWidgets.customDropDownSettingItem(settingsLabel: "LABEL", initialValue: state.labelValue),
                     MyWidgets.customDropDownSettingItem(settingsLabel: "KEY", initialValue: state.keyValue),
                     MyWidgets.customDropDownSettingItem(settingsLabel: "TYPE", initialValue: state.typeValue),
-                    MyWidgets.customDropDownSettingItem(settingsLabel: "VALUE", initialValue: state.typeValue),
+                    MyWidgets.customDropDownSettingItem(settingsLabel: "VALUE", initialValue: state.valueValue),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        MyButtons.shortButton(backgroundColor: MyColors.blueShade1, onTap: (){}, buttonText: "SAVE"),
+                        MyButtons.shortButton(backgroundColor: MyColors.blueShade1, onTap: (){
+                          state.saveConfiguration();
+                          StateService.pop();
+                        }, buttonText: "SAVE"),
                         MyButtons.shortButton(backgroundColor: MyColors.blueShade1, onTap: (){
                           state.resetDropDown();
                           StateService.pop();
-                        }, buttonText: "DISCARD"),
+                        }, buttonText: "RESET"),
                       ],
                     ),
                     SizedBox(height: 50.h,)
